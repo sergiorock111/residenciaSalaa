bfbd7c28d6f3e566e6e9ed0a6ed4fbb3770d2975
sergiorock111 <sssantos.ep@uesc.br>
2023-09-05 17:06:36 -0300

    atividades do dia 5/09

---
+++ atividade18.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void resto10(int &k){
+    if(k>=10)
+        k=k%10;
+        cout<<"valor de k"<<" "<<k;
+}
+
+int main(void){
+    int A;
+    cout<<"digite um valor";
+    cin>>A;
+    resto10(A);
+    cout<<"o novo valor: "<<" "<< A; 
+
+    return 0;
+}
